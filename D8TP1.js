bc6d86c40c6a42392ed6ccf561b84e73404c0250
Gabriel Titeux <gabititeux@gmail.com>
2021-10-28 11:18:43 +0200

    Commit test

---
+++ D8TP1.js
@@ -0,0 +1,24 @@
+
+let tab1 = [-1, 3, 10, 0]
+
+function verif(x){
+    for (let i = 0; i < x.length; i++){
+        if (x[i] >= 0 ){
+            console.log('True')
+        }
+        else{
+            console.log('False')
+        }
+
+
+    }
+}
+
+function dateWeek(){
+    let dw = new Date()
+    console.log(dw)
+}
+
+function nameTest(){
+
+}
